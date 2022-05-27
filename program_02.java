5ed577d464fd8578ba53b2d424f1337e04e87ea8
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-05-27 11:40:33 +0300

    modify program_02

--- program_02.java
+++ program_02.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /*
  * Пример 5
  * входные данные находятся в файле input.txt в виде
@@ -7,7 +11,30 @@
  * 1000
 */
 public class program_02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        double result = 0f;
+        int value = 0;
+        int pow = 0;
+        int[] temp = new int[2];
+        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+        for (int index = 0; index < 2; index++) {
+            temp[index] = Integer.parseInt(in.readLine());
+        }
+
+        value = temp[0];
+        pow = temp[1];
+        result = Math.pow(value, pow);
+
+        System.out.println(result);
+        in.close();
+
+        try (FileWriter out = new FileWriter("output.txt", false)) {
+            out.write(Double.toString(result));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
     }
 }
