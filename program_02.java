36570e22854e1601b2b48494fa940d9b55ae78c4
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-05-27 10:30:52 +0300

    add program_02

---
+++ program_02.java
@@ -0,0 +1,13 @@
+/*
+ * Пример 5
+ * входные данные находятся в файле input.txt в виде
+ * b 3
+ * a 10
+ * Результат нужно сохранить в файле output.txt
+ * 1000
+*/
+public class program_02 {
+    public static void main(String[] args) {
+
+    }
+}
