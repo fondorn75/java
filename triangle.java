0dd85deefd344abed3e0c52d271a651604cca167
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-06-01 14:20:33 +0300

    modify triangle

--- triangle.java
+++ triangle.java
@@ -1,19 +1,38 @@
+import java.io.FileWriter;
+
 /**
  * triangle
  */
 public class triangle {
 
     public static void main(String[] args) {
-        int temp = 0;
-        int n = 10;
-        String result = "";
+        number nt = new number();
+        nt.temp = 0;
+        nt.n = 10;
+        nt.result = "";
+        System.out.println(nt.numberTriangle());
+        try (
+                FileWriter out = new FileWriter("triangle.txt", false)) {
+            out.write(nt.result);
 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
+
+class number {
+    int n;
+    int temp;
+    String result;
+
+    String numberTriangle() {
         for (int i = 1; i < n; i++) {
             temp = (i * (i + 1)) / 2;
             result += temp + " ";
-
-            System.out.println(result);
         }
 
+        return result;
     }
 }
