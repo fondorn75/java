077417e99cd61fc9e700c0ba011ab6b1212a5d79
Dmitrii Dobrin <zatoichi.cadzicu@gmail.com>
2022-06-05 21:32:39 +0300

    modify Hanoi Towers

--- tower.java
+++ tower.java
@@ -2,16 +2,18 @@ public class tower {
     public static void main(String[] args) {
         Hanoi ht = new Hanoi();
         ht.Towers("1", "3", "2", 3);
+        
     }
 }
 
 
 class Hanoi{
 
-    String Towers(String with, String on, String some, int count){
+    void Towers(String with, String on, String some, int count){
         if (count > 1) Towers(with, on, some, count - 1);
         String result = with+">>"+on;
-        return result;
+        System.out.println(result);
+        if (count > 1) Towers(some, on, with, count - 1);
     }
 
 
