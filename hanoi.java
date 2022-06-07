e53dcc5d734c054d455875f1c15a54ba0c059550
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-06-07 12:08:49 +0300

    add hanoi

---
+++ hanoi.java
@@ -0,0 +1,16 @@
+public class hanoi {
+
+    static void Towers(String with, String on, String some, int count) {
+
+        if (count > 1)
+            Towers(with, some, on, count - 1);
+        System.out.println(with + " -> " + on);
+        if (count > 1)
+            Towers(some, on, with, count - 1);
+    }
+
+    public static void main(String[] args) {
+        Towers("1", "3", "2", 3);
+
+    }
+}
