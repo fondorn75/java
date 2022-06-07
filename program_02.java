ace2f184eccae42131acd91e515f67e70d075231
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-06-07 14:04:09 +0300

    modify pow

--- program_02.java
+++ program_02.java
@@ -11,30 +11,52 @@ import java.io.FileWriter;
  * 1000
 */
 public class program_02 {
-    public static void main(String[] args) throws Exception {
 
-        double result = 0f;
-        int value = 0;
-        int pow = 0;
-        int[] temp = new int[2];
-        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
-        for (int index = 0; index < 2; index++) {
-            temp[index] = Integer.parseInt(in.readLine());
-        }
-
-        value = temp[0];
-        pow = temp[1];
-        result = Math.pow(value, pow);
-
-        System.out.println(result);
-        in.close();
+    static void outputInFile(double temp) {
 
         try (FileWriter out = new FileWriter("output.txt", false)) {
-            out.write(Double.toString(result));
+            out.write(Double.toString(temp));
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        MyPow mp = new MyPow();
+        int[] temp = new int[2];
+        double tempres = 0f;
+
+        BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+        for (int index = 0; index < 2; index++) {
+            temp[index] = Integer.parseInt(in.readLine());
+        }
+
+        mp.value = temp[0];
+        mp.pow = temp[1];
+
+        tempres = mp.resultPow();
+
+        System.out.println(tempres);
+        in.close();
+
+        outputInFile(tempres);
+
+    }
+}
+
+class MyPow {
+    double result = 0f;
+    int value = 0;
+    int pow = 0;
+
+    double resultPow() {
+
+        result = Math.pow(value, pow);
+
+        return result;
+
     }
 }
