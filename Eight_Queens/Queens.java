b34d1bd7ea66264a9fe8f384e61905084cc7785c
Dmitrii Dobrin <zatoichi.cadzicu@gmail.com>
2022-06-25 20:20:16 +0300

    modify queens

--- Eight_Queens/Queens.java
+++ Eight_Queens/Queens.java
@@ -1,9 +1,9 @@
 public class Queens {
-    private int[] column; // Есть ли ферзь в том же столбце, 1 означает, что есть
-    private int[] rup; // Есть ли ферзь сверху справа налево снизу
-    private int[] lup; // Есть ферзь сверху слева направо снизу
-    private int[] queen; // ответ
-    private int num; // Номер решения
+    private int[] column; 
+    private int[] rup; 
+    private int[] lup; 
+    private int[] queens; 
+    private int num; 
 
     public Queens() {
         column = new int[8 + 1];
@@ -12,20 +12,20 @@ public class Queens {
         for (int i = 1; i <= 8; i++)
             column[i] = 0;
         for (int i = 1; i <= (2 * 8); i++)
-            rup[i] = lup[i] = 0; // Исходное определение - все без ферзя
-        queen = new int[8 + 1];
+            rup[i] = lup[i] = 0; 
+        queens = new int[8 + 1];
     }
 
-    public void backtrack(int i) {
+    public void turnBack(int i) {
         if (i > 8) {
             showAnswer();
         } else {
             for (int j = 1; j <= 8; j++) {
                 if ((column[j] == 0) && (rup[i + j] == 0) && (lup[i - j + 8] == 0)) {
-                    // Если ферзя нет
-                    queen[i] = j; // Установить как занятый
+                    
+                    queens[i] = j; 
                     column[j] = rup[i + j] = lup[i - j + 8] = 1;
-                    backtrack(i + 1); // Вызов цикла
+                    turnBack(i + 1); 
                     column[j] = rup[i + j] = lup[i - j + 8] = 0;
                 }
             }
@@ -37,7 +37,7 @@ public class Queens {
         System.out.println("\nОтвет: " + num);
         for (int y = 1; y <= 8; y++) {
             for (int x = 1; x <= 8; x++) {
-                if (queen[y] == x) {
+                if (queens[y] == x) {
                     System.out.print("Q");
                 } else {
                     System.out.print(".");
@@ -49,6 +49,6 @@ public class Queens {
 
     public static void main(String[] args) {
         Queens queen = new Queens();
-        queen.backtrack(1);
+        queen.turnBack(1);
     }
 }
